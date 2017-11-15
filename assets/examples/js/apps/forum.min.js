00250e1bb3bd6826a09cc13c117baa8613dfef53
yzsunlei <sun846990846>
2017-11-15 21:07:23 +0800

    update

---
+++ assets/examples/js/apps/forum.min.js
@@ -0,0 +1,6 @@
+/*!
+ * Remark (http://getbootstrapadmin.com/remark)
+ * Copyright 2016 amazingsurge
+ * Licensed under the Themeforest Standard Licenses
+ */
+!function(document,window,$){"use strict";window.AppForum=App.extend({run:function(next){next()}}),$(document).ready(function(){AppForum.run()})}(document,window,jQuery);
